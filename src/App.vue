c5842083d4d1a368bd2dbae143785d9ff30c5c9d
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-28 16:18:26 -0500

    Implementando Boilerplate(plantilla) para el proyecto

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,6 @@
     <router-view />
   </div>
 </template>
+
+<style lang="postcss" src="./assets/tailwind.postcss"></style>
+<style lang="css" src="./assets/main.css"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,46 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import HomePage from '../views/HomePage.vue';
+import SearchPage from '../views/SearchPage.vue';
+import NotFoundPage from '../views/NotFoundPage.vue';
+
+// User Pages
+import ProfilePage from '../views/user/ProfilePage.vue';
+import HousesPages from '../views/user/HousesPage.vue';
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: '/',
+    name: 'HomePage',
+    component: HomePage,
+  },
+  {
+    path: '/search',
+    name: 'SearchPage',
+    component: SearchPage,
+  },
+  {
+    path: '/user',
+    redirect: { name: 'ProfilePage' },
+  },
+  {
+    path: '/user/profile',
+    name: 'ProfilePage',
+    component: ProfilePage,
+  },
+  {
+    path: '/user/houses',
+    name: 'HousesPages',
+    component: HousesPages,
+  },
+  {
+    path: '*',
+    name: 'NotFoundPage',
+    component: NotFoundPage,
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
