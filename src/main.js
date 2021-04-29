2e4da594970ea32c6194ae5d7240ae2c103a02d3
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-29 15:55:04 -0500

    Instalación y Configuracion de Firebase Realtime Database

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 import Vue from 'vue';
+import firebase from 'firebase'; // Agregar firebase antes de App.vue
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBgJhhxlz1S81lmMYoew_rT8CXe-Jftpsg',
+  authDomain: 'platzi-room-9158a.firebaseapp.com',
+  databaseURL: 'https://platzi-room-9158a-default-rtdb.firebaseio.com',
+  projectId: 'platzi-room-9158a',
+  storageBucket: 'platzi-room-9158a.appspot.com',
+  messagingSenderId: '770101165592',
+  appId: '1:770101165592:web:c7fff85da3d50098a48d6d',
+  measurementId: 'G-8ELRFF2WFH',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,
--- src/views/user/HousesPage.vue
+++ src/views/user/HousesPage.vue
@@ -5,7 +5,7 @@
         <form class="form">
           <div class="form__field relative">
             <i class="input-icon material-icons absolute text-grey-darker">search</i>
-            <input class="input__search" id="where" type="text" placeholder="Mexico City, Mexico">
+            <input class="input__search" id="where" type="text" placeholder="Mexico City, Mexico" />
           </div>
         </form>
       </div>
@@ -16,7 +16,10 @@
         <div class="grid-container">
           <div class="house__card mb-3" v-for="i in 4" :key="i">
             <div class="house__thumbnail relative overflow-hidden">
-              <img class="house__image absolute w-full" src="https://images.unsplash.com/photo-1432303492674-642e9d0944b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250&q=80">
+              <img
+                class="house__image absolute w-full"
+                src="https://images.unsplash.com/photo-1432303492674-642e9d0944b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250&q=80"
+              />
             </div>
             <div class="house__content bg-white p-3 border">
               <div class="house__type font-semibold text-xs uppercase text-teal-dark mb-1">
@@ -51,23 +54,22 @@ export default {
   grid-template-columns: repeat(4, 1fr);
 }
 
+@media (max-width: 992px) {
+  .house__card > .house__thumbnail {
+    height: 150px;
+  }
+  .section__houses .grid-container {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
 
-  @media(max-width: 992px) {
-    .house__card > .house__thumbnail {
-      height: 150px;
-    }
-    .section__houses .grid-container {
-      grid-template-columns: repeat(3, 1fr);
-    }
+@media (max-width: 576px) {
+  .section__houses .grid-container {
+    grid-template-columns: repeat(1, 1fr);
   }
 
-  @media(max-width: 576px) {
-    .section__houses .grid-container {
-      grid-template-columns: repeat(1, 1fr);
-    }
-
-    .house__card > .house__thumbnail {
-      height: 120px;
-    }
+  .house__card > .house__thumbnail {
+    height: 120px;
   }
+}
 </style>
