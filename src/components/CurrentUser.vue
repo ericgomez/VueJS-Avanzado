0947abe44f589fd40fc693290df57f058ba5f9cf
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-30 11:57:37 -0500

    Integración de Scoped Slots en Platzi Rooms

---
+++ src/components/CurrentUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="current-user">
+    <!-- slot principal - informacion que lanza hacia el componente padre  -->
+    <slot :user="user"></slot>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'CurrentUser',
+  computed: {
+    // Accedemos al Helper a travez del Spread Operator
+    ...mapGetters({
+      // le indicamo que la variable user este enlazado con authUser
+      user: 'authUser',
+    }),
+  },
+};
+</script>
--- src/partials/HeaderPartial.vue
+++ src/partials/HeaderPartial.vue
@@ -10,47 +10,51 @@
           >
         </div>
         <div class="flex items-center w-auto">
-          <div class="items__controls">
-            <div class="flex" v-if="user">
-              <router-link
-                :to="{ name: 'CreateHousePage' }"
-                class="no-underline mr-2 flex items-center text-grey-darkest"
-              >
-                <i class="material-icons">add</i>
-              </router-link>
-              <button class="mr-4 flex items-center">
-                <i class="material-icons">notifications</i>
-              </button>
-              <div class="flex items-center mr-4">
-                <img
-                  class="w-8 h-8 rounded-full mr-2"
-                  src="https://avatars2.githubusercontent.com/u/1901273?s=460&v=4"
-                  alt="Avatar of Javier Diaz"
-                />
-                <div class="text-sm">
-                  <p class="text-black leading-none">{{ user.name }}</p>
-                  <p class="text-grey-dark">Online</p>
+          <current-user>
+            <template slot-scope="{ user }">
+              <div class="items__controls">
+                <div class="flex" v-if="user">
+                  <router-link
+                    :to="{ name: 'CreateHousePage' }"
+                    class="no-underline mr-2 flex items-center text-grey-darkest"
+                  >
+                    <i class="material-icons">add</i>
+                  </router-link>
+                  <button class="mr-4 flex items-center">
+                    <i class="material-icons">notifications</i>
+                  </button>
+                  <div class="flex items-center mr-4">
+                    <img
+                      class="w-8 h-8 rounded-full mr-2"
+                      src="https://avatars2.githubusercontent.com/u/1901273?s=460&v=4"
+                      alt="Avatar of Javier Diaz"
+                    />
+                    <div class="text-sm">
+                      <p class="text-black leading-none">{{ user.name }}</p>
+                      <p class="text-grey-dark">Online</p>
+                    </div>
+                  </div>
+                  <button class="flex items-center">
+                    <i class="material-icons" @click.prevent="logOut">exit_to_app</i>
+                  </button>
+                </div>
+                <div v-else>
+                  <button
+                    class="btn__outline btn__outline--teal rounded mr-2"
+                    @click.prevent="getLogin"
+                  >
+                    Login
+                  </button>
+                  <button
+                    class="bg-yellow-dark text-yellow-darker font-semibold py-2 px-4 rounded"
+                    @click.prevent="signUp"
+                  >
+                    Register
+                  </button>
                 </div>
               </div>
-              <button class="flex items-center">
-                <i class="material-icons" @click.prevent="logOut">exit_to_app</i>
-              </button>
-            </div>
-            <div v-else>
-              <button
-                class="btn__outline btn__outline--teal rounded mr-2"
-                @click.prevent="getLogin"
-              >
-                Login
-              </button>
-              <button
-                class="bg-yellow-dark text-yellow-darker font-semibold py-2 px-4 rounded"
-                @click.prevent="signUp"
-              >
-                Register
-              </button>
-            </div>
-          </div>
+            </template>
+          </current-user>
         </div>
       </div>
     </div>
@@ -58,15 +62,10 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import CurrentUser from '@/components/CurrentUser.vue';
 
 export default {
   name: 'HeaderPartial',
-  data() {
-    return {
-      isAuthenticated: false,
-    };
-  },
   methods: {
     getLogin() {
       this.$store.dispatch('TOGGLE_MODAL_STATE', {
@@ -84,12 +83,8 @@ export default {
       this.$store.dispatch('LOG_OUT');
     },
   },
-  computed: {
-    // Accedemos al Helper a travez del Spread Operator
-    ...mapGetters({
-      // le indicamo que la variable user este enlazado con authUser
-      user: 'authUser',
-    }),
+  components: {
+    CurrentUser,
   },
 };
 </script>
